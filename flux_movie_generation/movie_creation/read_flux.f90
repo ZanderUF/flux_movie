a170a4c17bd0cb0e071b1c9f99efe41f8de94c47
Zander Mausolff <zanderm@zmac.ad.ufl.edu>
2016-02-17 11:08:02 -0500

    oranized folders

--- flux_movie_generation/movie_creation/read_flux.f90
+++ flux_movie_generation/movie_creation/read_flux.f90
@@ -8,10 +8,10 @@ program create_movie
 
 implicit none
 !------------Variables---------------!
-integer ::  engp,incr,stgp,stat,k,kmax,ngp,iflux
+integer ::  ig,engp,incr,stgp,stat,k,kmax,ngp,iflux
 real :: time,klgkeff
 character(10) :: datafilename
-real  :: flux_array(44,238)
+real  :: flux_array(44,238), tot_flux_ar(44)
 character(len=100) :: line
 
 iflux = 10
@@ -22,10 +22,22 @@ datafilename = "flux.for.0"
         read(iflux) time,klgkeff
         print *, kmax,ngp
         print *,time,klgkeff
-
+        tot_flux_ar(k) = 0.0
+        flux_array(k,ig) = 0.0
+        ngp = 238
+        kmax = 44
+        
         do k=1,kmax
-            read(iflux) flux_array(k,238),ngp,engp,incr  
-            print *, flux_array(k,238),ngp,engp,incr 
+            read(iflux) flux_array(k,1:ngp)
+            do ig=1,ngp
+                tot_flux_ar(k) = flux_array(k,ig) + tot_flux_ar(k)
+            end do
         end do
+        
+        do k=1,kmax
+                print *, tot_flux_ar(k)
+        end do
+!---loop over  and add all flux valus for a given region-!
+        
 
 end program
